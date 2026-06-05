72fe1d8be14a922c8eb3a7c459864c16ab1509e5
call-me-prof <OpFaisal4@hotmail.com>
2026-06-05 20:18:17 +0000

    Fix task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import hidden_4
+
 if __name__ == "__main__":
-    from variable_load_5 import a
-    print("{}".format(a))
+    for name in dir(hidden_4):
+        if not name.startswith("__"):
+            print(name)
