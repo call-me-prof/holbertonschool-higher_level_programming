4b0451a419ec04d45ab80824e7b5eb8651fefae2
call-me-prof <OpFaisal4@hotmail.com>
2026-06-05 19:37:01 +0000

    Fix syntax and pycodestyle for tasks 0, 2, and 4

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    count = len(sys.argv) - 1
+
+    argv = sys.argv[1:]
+    count = len(argv)
+
     if count == 0:
         print("0 arguments.")
     elif count == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
-    
-    for i in range(1, count + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+
+    for i, arg in enumerate(argv, start=1):
+        print("{}: {}".format(i, arg))
