ed3cadbebf9c2986f3da69a0202cd6e7d77616a6
call-me-prof <OpFaisal4@hotmail.com>
2026-06-18 19:53:08 +0000

    Implement Rectangle tasks 0 through 9 with all requirements

---
+++ python-classes/7-rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""Defines a Rectangle with customizable printing symbol."""
+
+
+class Rectangle:
+    """Represents a rectangle."""
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle."""
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """Get/set the width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the perimeter."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return the printable representation using print_symbol."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        symbol_str = str(self.print_symbol)
+        rect_lines = [symbol_str * self.__width for _ in range(self.__height)]
+        return "\n".join(rect_lines)
+
+    def __repr__(self):
+        """Return the reproduction string."""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Decrement counter and print a message upon deletion."""
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
