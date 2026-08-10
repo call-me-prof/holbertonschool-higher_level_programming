473002cda832a6c58b217b7cc023ed75a2d0f66a
FAISAL ALSHAHRANI <call-me-prof>
2026-08-10 11:29:49 +0300

    Add Task 1 Jinja template implementation

---
+++ python-server_side_rendering/task_01_jinja.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+# Route for the Home page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Route for the About page
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Route for the Contact page
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
